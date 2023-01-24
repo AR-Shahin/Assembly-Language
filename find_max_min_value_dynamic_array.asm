29a296311647feb639adb1aa39aee494e9f53cb9
AR Shahin <56718222+AR-Shahin@users.noreply.github.com>
2023-01-24 19:45:32 +0600

    Update find_max_min_value_dynamic_array.asm

--- find_max_min_value_dynamic_array.asm
+++ find_max_min_value_dynamic_array.asm
@@ -47,3 +47,27 @@ L2: MOV AL,BL
 LOOP L1
 
 
+------------------------------ dynamic way ---------------------------------
+ORG 100H
+
+.DATA    
+    ARR DB 10,3,4,-1,51,40,-80,9
+    LEN EQU $ -ARR
+.CODE
+
+LEA SI,ARR
+MOV CX,LEN 
+DEC CX
+MOV AL,[SI]
+
+L1: INC SI
+    MOV BL,[SI]
+    CMP BL,AL
+    JL L2  
+LOOP L1
+
+HLT
+
+L2: MOV AL,BL     
+
+LOOP L1
